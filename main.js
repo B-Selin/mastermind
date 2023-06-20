de2e986e8eafd8aebcb76b6a524db885be5fafd9
B-Selin <selin@bozkurt.ca>
2023-06-20 11:45:13 -0600

    GenerateSecretCode and getWinner functions

--- main.js
+++ main.js
@@ -6,15 +6,24 @@ const allGuesses = ['0','1','2','3','4','5','6','7','8','9'];
 /*----- state variables -----*/
 let numGuesses;
 let secretCode;
-let matches;
+let matchesInPlace = 0;
+let matchesNotInPlace = 0;
 let playerGuess;
-let contimuePlaying;
-let winner;
+let contimuePlaying = true;
+let winner = false;
+let usedNumbers= [];
 
 /*----- cached elements  -----*/
+let codeEl;
+let guessEl;
+let guessCountEl;
+let messageEl;
+let playAgainEl;
+let resultEl; // Not sure if I will need this
 
 
 /*----- event listeners -----*/
+
 
 
 /*----- functions -----*/
@@ -27,6 +36,16 @@ function render(){
 }
 
 function generateSecretCode(){
+  // secret code is initially equals to an empty array
+  secretCode = [];
+
+  //I dod not hardcoded length of the secret code & possible guesses
+  for (let i = 0; i < secretCodeLength; i++) { 
+    // generate a random number from 0 - 9
+    let randomIdx = Math.floor(Math.random() * allGuesses.length);
+    //Push four random numbers to the empty array
+    secretCode.push(allGuesses[randomIdx]);
+  }
 
 }
 
@@ -39,7 +58,9 @@ function checkPlayerGuess(){
 }
 
 function getWinner(){
-
+  if (matchesInPlace === secretCodeLength){
+    winner = true;
+  }
 }
 
 function playAgain(){
