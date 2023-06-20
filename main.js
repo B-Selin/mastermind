a5b7fd76723c7a02dcaee4aca0583dd7d802c935
B-Selin <selin@bozkurt.ca>
2023-06-20 11:10:09 -0600

    Basic js with constants and initial functions

---
+++ main.js
@@ -0,0 +1,48 @@
+/*----- constants -----*/
+const maxGuess = 5;
+const secretCodeLength = 4;
+const allGuesses = ['0','1','2','3','4','5','6','7','8','9'];
+
+/*----- state variables -----*/
+let numGuesses;
+let secretCode;
+let matches;
+let playerGuess;
+let contimuePlaying;
+let winner;
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+function init(){
+
+}
+
+function render(){
+
+}
+
+function generateSecretCode(){
+
+}
+
+function getPlayerGuess(){
+
+}
+
+function checkPlayerGuess(){
+
+}
+
+function getWinner(){
+
+}
+
+function playAgain(){
+
+}
+
