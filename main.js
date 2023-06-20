d568e480f1b626fc300e446d1899141e57c762a6
B-Selin <selin@bozkurt.ca>
2023-06-20 16:22:26 -0600

    init function detailed

--- main.js
+++ main.js
@@ -4,37 +4,54 @@ const secretCodeLength = 4;
 const allGuesses = ['0','1','2','3','4','5','6','7','8','9'];
 
 /*----- state variables -----*/
+let board;
 let numGuesses;
 let secretCode;
-let matchesInPlace = 0;
-let matchesNotInPlace = 0;
+let matchesInPlace;
+let matchesNotInPlace;
 let playerGuess;
-let contimuePlaying = true;
-let winner = false;
-let usedNumbers= [];
+let continuePlaying;
+let winner;
+let usedNumbers;
 
 /*----- cached elements  -----*/
 let codeEl;
 let guessEl;
 let guessCountEl;
-let messageEl;
-let playAgainBtn;
+const infoEl = document.querySelector('info');
+const playAgainBtn = document.querySelector('button');
 let resultEl; // Not sure if I will need this
 
 
 /*----- event listeners -----*/
 //An event listener for play again button to restart the game with init() function
-playAgainBtn.addEventListener('click', init );
+playAgainBtn.addEventListener('click', init); //provide the function, do not invoke the function
 
+//An event listener to get player guess
+guessEl.addEventListener('click', handleDrop)
 
 /*----- functions -----*/
+init();//invoke init function to start the game
+
 function init(){
-
+  board = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ];
+  winner = false;
+  numGuesses = 0;
+  matchesInPlace = 0;
+  matchesNotInPlace = 0;
+  continuePlaying = true;
+  usedNumbers = [];
+  render();
 }
 
-function render(){
 
-}
 
 function generateSecretCode(){
   // secret code is initially equals to an empty array
@@ -55,7 +72,6 @@ function getPlayerGuess(){
 }
 
 function checkPlayerGuess(){
-  //
 
 }
 
@@ -66,7 +82,12 @@ function getWinner(){
     winner = true;
   }
 }
+function render(){
+  //render board
+  //render information
+  //render diamonds - this is optional at this point
 
+}
 function playAgain(){
 
 }
